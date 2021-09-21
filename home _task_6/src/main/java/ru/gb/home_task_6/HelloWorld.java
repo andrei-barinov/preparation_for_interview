8dd956e8a6931b3d4934debfe2ac19496fa86e15
Andre <ncl8@mail.ru>
2021-09-21 18:14:32 +0300

    version 1.0

---
+++ home _task_6/src/main/java/ru/gb/home_task_6/HelloWorld.java
@@ -0,0 +1,26 @@
+package ru.gb.home_task_6;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello/*")
+public class HelloWorld extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws IOException, ServletException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello World!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello World1</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
